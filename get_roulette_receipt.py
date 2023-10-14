19ac58109ea9fa6fccbb9df0f6fdab2487a319cb
DeFiPages <defichainpages@gmail.com>
2023-10-14 13:57:47 +0200

    receipt as batch

--- get_roulette_receipt.py
+++ get_roulette_receipt.py
@@ -2,8 +2,50 @@ from libs.my_eth_utils import w3, roulette_address
 from libs.eth_batch import send_batch_json_rpc_request
 import sys
 
+def get_batch_blocks(from_block, to_block):
+    method = 'eth_getBlockByNumber'
+    params_list = [[hex(block_number), True] for block_number in range(from_block, to_block + 1)]
+    endpoint = w3.provider.endpoint_uri
+    response_data = send_batch_json_rpc_request(endpoint, method, params_list)
+    if 'error' in response_data:
+        print(response_data)
+    return response_data
 
-def get_block_transactions(from_block, to_block, contract_address):
+def extract_relevant_txs(batch_blocks, contract_address):
+    relevant_txs = {}
+    for block_data in batch_blocks:
+        if isinstance(block_data, dict):
+            if 'result' in block_data:
+                block = block_data['result']
+                block_number = int(block['number'], 16)  # Convert hex to int
+                tx_hashes = [tx['hash'] for tx in block['transactions'] if tx['to'] == contract_address]
+                if tx_hashes:
+                    relevant_txs[block_number] = {
+                        "total_tx_count": len(tx_hashes),
+                        "tx_hashes": tx_hashes,
+                    }
+            else:
+                print(f"Error for block: {block_data.get('error', {}).get('message', 'Unknown error')}")
+        else:
+            print(f"Unexpected block data type. Received: {block_data}")
+    return relevant_txs
+
+
+
+def get_block_transactions(from_block, to_block, contract_address, batch_size=100):
+    block_transactions = {}
+    for start_block in range(from_block, to_block + 1, batch_size):
+        end_block = min(start_block + batch_size - 1, to_block)
+
+        batch_blocks = get_batch_blocks(start_block, end_block)
+        relevant_txs = extract_relevant_txs(batch_blocks, contract_address)
+        block_transactions.update(relevant_txs)
+
+        print(f"Processed blocks {start_block} to {end_block}.")
+    return block_transactions
+
+
+def get_block_transactions_(from_block, to_block, contract_address):
     block_transactions = {}
     for block_number in range(from_block, to_block + 1):
         if block_number % 100 == 0:
@@ -34,7 +76,7 @@ def get_batch_transaction_receipts(transaction_hashes):
     return receipts
 
 # Specify the block range
-from_block = 1000 # Example starting block number
+from_block = 1500 # Example starting block number
 to_block = w3.eth.block_number
 
 # Get all transaction hashes for each block for the specified contract address in the block range
